33069b098ccb301525dc03d5cee5b285a58b466c
Tanapatjeensamran <imanuglypanda@gmail.com>
2022-04-24 16:01:57 +0700

    ready

--- index.js
+++ index.js
@@ -5,11 +5,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World! On Cloud!2");
+  res.send("Hello World! On Cloud!");
 });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
-
-// Changes
